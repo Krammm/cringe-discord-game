bf6cfef246446d30dee475262de9debe2b486089
Marco <42967229+Krammm@users.noreply.github.com>
2020-08-01 18:57:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,29 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
 
-const gameWidth = 16
-const gameHeight = 9
+ 
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+
+    console.log('I am ready!');
+
 });
 
-client.on('message', msg => {
-  msg = msg.toLowerCase();
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-  
-  
-  
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
 });
 
+ 
 
+// THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.BOT_TOKEN); //BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
