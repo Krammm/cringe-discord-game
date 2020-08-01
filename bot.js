dd968b835a6f345c5f9ca1602ca9dd62a3165f35
Marco <42967229+Krammm@users.noreply.github.com>
2020-08-01 18:34:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,61 +1,14 @@
 const Discord = require('discord.js');
-// boh
 const client = new Discord.Client();
-const prefix = '!';
+
 client.on('ready', () => {
-
-    console.log('I am ready!');
-
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', message => {
-
-    if (message.author.id === client.user.id) { return; }
-
-    if (message.isMemberMentioned(client.user)) {
-
-    }
-
-    command = message.content.toLowerCase()
-
-    if (command.startsWith(prefix)) { 
-        var line = command;
-        line = line.substring(1);
-        var args = message.content.trim().split(/ +/g);
-        command = args[0];
-        command = command.substring(1);
-        var user = message.mentions.members.first();
-
-        switch (command) {
-            case 'removerole':
-                try {
-                    user.removeRole(role);
-                    message.channel.send('Ho tolto ' + user + " dal ruolo " + role + ".");
-                } catch (error) {
-                    console.log(error);
-                    message.channel.send("Sono troppo insicura per farlo...");
-                    message.channel.send("ah, è ho anche riscontrato un errore");
-                    //message.channel.send("@Krammm , @NinjaSbrillo non sapete neanche programmarmi")
-                }
-                break;
-            case 'clear':
-                
-                break;
-            default:
-                message.channel.send("Command: " + command + " Line: " + line);
-                break;
-        }
-    }
-
-
-
-
-
- 
-
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
-
-
-// THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN); //BOT_TOKEN is the Client Secret
