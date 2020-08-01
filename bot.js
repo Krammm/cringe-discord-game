866f385750cfd64369a309df87ea32b94e8081de
Marco <42967229+Krammm@users.noreply.github.com>
2020-08-01 18:40:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,23 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const gameWidth = 16
+const gameHeight = 9
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
+  msg = msg.toLowerCase();
   if (msg.content === 'ping') {
     msg.reply('Pong!');
   }
+  
+  
+  
 });
+
+
 
 client.login(process.env.BOT_TOKEN); //BOT_TOKEN is the Client Secret
